2492fcb2eec3dbfe346bd9aba18129fd6d3de8eb
ibrahimramze <shikhhui@sheridancollege.ca>
2020-01-22 13:36:46 -0500

    WEeek1

---
+++ src/week3test/Week3Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3test;
+
+/**
+ *
+ * @author ibrah
+ */
+public class Week3Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("gfdsdgsgsdgdssdf");
+    }
+    
+}
